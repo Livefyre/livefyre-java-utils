b955a311c7831731a6c6be955ea2e4c048dc6774
Vishwesh Joshi <vishwesh.joshi@tudip.nl>
2018-02-28 10:52:33 +0530

    Removed the ternary operator for the http call. Added the @Deprecated to setSsl.

--- src/main/java/com/livefyre/api/Domain.java
+++ src/main/java/com/livefyre/api/Domain.java
@@ -7,13 +7,11 @@ import com.livefyre.utils.LivefyreUtil;
 public class Domain {
     public static String quill(LfCore core) {
         Network network = LivefyreUtil.getNetworkFromCore(core);
-        return network.isSsl() ? String.format("https://%s.quill.fyre.co",
-                network.getNetworkName()) : String.format("http://quill.%s.fyre.co", network.getNetworkName());
+        return String.format("https://%s.quill.fyre.co", network.getNetworkName());
     }
-    
+
     public static String bootstrap(LfCore core) {
         Network network = LivefyreUtil.getNetworkFromCore(core);
-        return network.isSsl() ? String.format("https://%s.bootstrap.fyre.co",
-                network.getNetworkName()) : String.format("http://bootstrap.%s.fyre.co", network.getNetworkName());
+        return String.format("https://%s.bootstrap.fyre.co", network.getNetworkName());
     }
 }
